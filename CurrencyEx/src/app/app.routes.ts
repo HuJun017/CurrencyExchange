e0a81122846811bc150c2d885b1058ac742ccc24
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-11-11 10:32:26 +0000

    routes

--- CurrencyEx/src/app/app.routes.ts
+++ CurrencyEx/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Conversion } from './components/conversion/conversion';
+import { Storico } from './components/storico/storico';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    //path per le pagine di conversione e lo storico delle valute
+    { path: 'conversion', component: Conversion },
+    { path: 'storico', component: Storico }
+
+    //se la path è vuota non va su nessuna delle due, perchè rimane sulla homepage
+]
