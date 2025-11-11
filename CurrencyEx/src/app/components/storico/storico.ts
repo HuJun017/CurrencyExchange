89c053a02681b6bc03e85cd0f566020471693d66
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-11-11 10:16:11 +0000

    inizializzazione storico

---
+++ CurrencyEx/src/app/components/storico/storico.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storico',
+  imports: [],
+  templateUrl: './storico.html',
+  styleUrl: './storico.css',
+})
+export class Storico {
+
+}
