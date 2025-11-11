56a086d71383381e9fd7865194258b114f69a6f3
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-11-11 10:14:51 +0000

    inizializzazione conversion

---
+++ CurrencyEx/src/app/components/conversion/conversion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversion',
+  imports: [],
+  templateUrl: './conversion.html',
+  styleUrl: './conversion.css',
+})
+export class Conversion {
+
+}
